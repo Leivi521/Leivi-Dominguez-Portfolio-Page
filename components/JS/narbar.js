315f7f7ea616b4b35e4cf44ac8857063920cea0e
Leivi Dominguez Bruno <165715355+Leivi521@users.noreply.github.com>
2025-01-19 20:14:01 +0000

    quicksave

--- components/JS/narbar.js
+++ components/JS/narbar.js
@@ -1,42 +1,40 @@
-function Navbar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="container">
-          <a className="navbar-brand" href="/">Leivi521</a>
-          
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarSupportedContent"
-          >
-            <div className="hamburger">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
-          </button>
-  
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="#home">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#skills">Skills</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">About Me</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">Contact</a>
-              </li>
-            </ul>
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">Leivi521</a>
+        
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarSupportedContent"
+        >
+          <div className="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#skills">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About Me</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">Contact</a>
+            </li>
+          </ul>
         </div>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+      </div>
+    </nav>
+  );
+};
